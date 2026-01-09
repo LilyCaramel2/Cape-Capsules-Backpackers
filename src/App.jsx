3efedaae465d449f4493ad5686329598c01e2608
Lily Caramel <131236664+LilyCaramel2@users.noreply.github.com>
2026-01-09 18:03:12 +0200

    Wire ValueProps section into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Hero from './sections/Hero'
+import ValueProps from './sections/ValueProps'
 import Footer from './components/Footer'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <ValueProps />
         {/* Additional sections will be added here */}
       </main>
       <Footer />
